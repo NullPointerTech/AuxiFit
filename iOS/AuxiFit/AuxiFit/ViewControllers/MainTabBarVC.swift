017696f9239922fd5840bba9e83a72a7bba6006d
Anjandeep Sahni <sahni.anjandeep@gmail.com>
2018-07-11 01:08:33 +0530

    Created basic application with navigation and tab bar VC.

---
+++ iOS/AuxiFit/AuxiFit/ViewControllers/MainTabBarVC.swift
@@ -0,0 +1,68 @@
+//
+//  MainTabBarVC.swift
+//  AuxiFit
+//
+//  Created by Anjandeep Sahni on 10/07/18.
+//  Copyright © 2018 Null Pointer Tech. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarVC: UITabBarController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Setup color for MainTabBarVC.
+        view.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        
+        // Assign self for delegate so that MainTabBarVC
+        // can respond to UITabBarControllerDelegate methods
+        self.delegate = self
+        
+        // Create Tab One
+        let tabOne = TabOneVC()
+        let tabOneBarItem = UITabBarItem(title: tabOne.getTabTitle(), image: nil, selectedImage: nil)
+        tabOne.tabBarItem = tabOneBarItem
+        
+        // Create Tab Two
+        let tabTwo = TabTwoVC()
+        let tabTwoBarItem = UITabBarItem(title: tabTwo.getTabTitle(), image: nil, selectedImage: nil)
+        tabTwo.tabBarItem = tabTwoBarItem
+        
+        // Create Tab Three
+        let tabThree = TabThreeVC()
+        let tabThreeBarItem = UITabBarItem(title: tabThree.getTabTitle(), image: nil, selectedImage: nil)
+        tabThree.tabBarItem = tabThreeBarItem
+        
+        // Create Tab Four
+        let tabFour = TabFourVC()
+        let tabFourBarItem = UITabBarItem(title: tabFour.getTabTitle(), image: nil, selectedImage: nil)
+        tabFour.tabBarItem = tabFourBarItem
+        
+        // Create Tab Five
+        let tabFive = TabFiveVC()
+        let tabFiveBarItem = UITabBarItem(title: tabFive.getTabTitle(), image: nil, selectedImage: nil)
+        tabFive.tabBarItem = tabFiveBarItem
+        
+        self.viewControllers = [tabOne, tabTwo, tabThree, tabFour, tabFive]
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS/AuxiFit/AuxiFit/ViewControllers/TabTwoVC.swift
@@ -0,0 +1,56 @@
+//
+//  TabTwoVC.swift
+//  AuxiFit
+//
+//  Created by Anjandeep Sahni on 10/07/18.
+//  Copyright © 2018 Null Pointer Tech. All rights reserved.
+//
+
+import UIKit
+
+class TabTwoVC: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+        //Setup tab title here so that over VC's can fetch.
+        self.title = "Nutrition"
+    }
+    
+    // This is required by compiler.
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Setup view controller color.
+        self.view.backgroundColor = UIColor(white: 0.55, alpha: 1)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func getTabTitle() -> String {
+        // Return the title for this tab.
+        var tabTitle = ""
+        if self.title != nil {
+            tabTitle = self.title!
+        }
+        return tabTitle
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
